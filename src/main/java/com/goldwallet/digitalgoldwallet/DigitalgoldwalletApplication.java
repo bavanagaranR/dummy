4d04228212f622bcbcb084aa26030418a7ee8ca7
bavanagaranR <bavanab42@gmail.com>
2026-04-13 21:20:39 +0530

    add project structure and application properties

---
+++ src/main/java/com/goldwallet/digitalgoldwallet/DigitalgoldwalletApplication.java
@@ -0,0 +1,13 @@
+package com.goldwallet.digitalgoldwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalgoldwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalgoldwalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldwallet/digitalgoldwallet/DigitalgoldwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldwallet.digitalgoldwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalgoldwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
